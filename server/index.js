b4667437494218c46ea5af7e247fa96c3a858f0c
kirilljazzsax <matsak_92@mail.ru>
2020-07-24 00:05:54 +0300

    changes

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require ('express');
 const fallback = require ('express-history-api-fallback');
 const fileUpload = require ('express-fileupload');
+const PORT = process.env.PORT || 3000;
 
 const app = express ();
 
@@ -10,6 +11,6 @@ app.use ('/api', require('./routes/api'));
 app.use (fileUpload())
 app.use (fallback ('index.html', { root: 'dist' }));
 
-app.listen (process.env.PORT || 3000, () => {
+app.listen (PORT, () => {
     console.log ('should work');
 })
