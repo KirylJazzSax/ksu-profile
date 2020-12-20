18060750a6d8e9ff4d0b26c6cd2de208ba6eac02
KirylJazzSax <matsak_92@mail.ru>
2020-12-20 20:06:44 +0300

    Change design for immersive

--- src/views/Immersive.vue
+++ src/views/Immersive.vue
@@ -1,30 +1,43 @@
 <template>
     <div class="immersive-container">
-      <div class="text"><span>{{ $t('immersive') }}</span></div>
+      <div class="immersive-text-with-picture">
+        <div class="ksu-immersive-picture">
+          <plane v-show="imageLoading"></plane>
+          <img class="image-hidden" src="../assets/immersive-pick.jpg" alt="">
+        </div>
+        <div class="immersive-text">
+          <div class="mb-3">{{ $t('immersive') }}</div>
+          <div class="list-group immersive-awards-list">
+            <a :class="{'list-group-item': true, 'list-group-item-action': true}"
+               target="_blank"
+               :href="award.link"
+               :key="index"
+               v-for="(award, index) in awards">
+              {{ award.name }}
+            </a>
+          </div>
+        </div>
+      </div>
       <div class="immersive-audios">
         <Audios :audios="audios" />
-      </div>
-      <div class="list-group immersive-awards-list">
-        <a :class="{'list-group-item': true, 'list-group-item-action': true}"
-           target="_blank"
-           :href="award.link"
-           :key="index"
-           v-for="(award, index) in awards">
-          {{ award.name }}
-        </a>
       </div>
     </div>
 </template>
 
 <script>
   import Audios from '../components/Audios'
+  import Plane from 'vue-loading-spinner/src/components/Plane'
     export default {
         name: "Immersive",
         components: {
-          Audios
+          Audios,
+          Plane
         },
         data() {
           return {
+            imageLoading: true,
+            percentage: 0,
+            breakPoint: 992,
             audios: [
               {
                 title: 'My audio',
@@ -57,30 +70,88 @@
         },
         mounted() {
           this.$el.style.maxHeight = 'fit-content'
+          this.percentage = ((4032 - 3024) / 4032) * 100
+          const ksuPickContainer = this.$el.querySelector('.ksu-immersive-picture')
+          const windowWidth = window.innerWidth
+          if (windowWidth <= this.breakPoint) {
+            // const calculatedHeight = Math.floor(windowWidth - (windowWidth * (this.percentage / 100)))
+            // ksuPickContainer.style.height = calculatedHeight + 'px'
+            ksuPickContainer.style.display = 'none'
+          }
+          const ksuPick = this.$el.querySelector('.image-hidden')
+          ksuPick.onload = () => {
+            if (windowWidth <= this.breakPoint) {
+              const ksuPickAndTextContainer = this.$el.querySelector('.immersive-text-with-picture')
+              ksuPickAndTextContainer.style.backgroundImage = `url('${ksuPick.src}')`
+            } else {
+              ksuPickContainer.style.backgroundImage = `url('${ksuPick.src}')`
+            }
+            this.imageLoading = false
+          }
         }
     }
 </script>
 
 <style lang="scss">
   .immersive-container {
-    background-image: url("../assets/immersive-pick.jpg");
-    background-size: cover;
-    padding: 20px;
-    font-size: 17px;
+    //background-image: url("../assets/immersive-pick.jpg");
+    //background-size: cover;
+    //padding: 20px;
+    //font-size: 17px;
+    background-color: #e4e4e3;
+    font-size: 15px;
 
-    .text {
-      background-color: rgba(0, 0, 0, 0.3);
-      color: #f8f9fa;
-      padding: 10px;
-    }
-    .list-group-item {
-      background-color: rgba(0, 0, 0, 0.4);
-      color: #f8f9fa;
+    .immersive-text-with-picture {
+      background-size: cover;
+      background-position: center;
+      .immersive-text {
+        text-align: justify;
+        width: 50%;
+        height: calc(100vh - 151px);
+        z-index: 1;
+        margin-left: auto;
+        padding: 30px;
 
-      &:hover {
-        background-color: rgba(255, 255, 255, 0.7);
-        cursor: pointer;
-        color: black;
+        @media screen and (max-width: 992px) {
+          width: 100%;
+          height: auto;
+          color: white;
+          padding: 15px;
+          background-color: rgba(0, 0, 0, 0.3);
+        }
+      }
+
+      .ksu-immersive-picture {
+        width: 50%;
+        height: calc(100vh - 151px);
+        /* background-image: url("../assets/ksu-back.jpg"); */
+        background-size: cover;
+        background-position: 47%;
+        position: absolute;
+
+        @media screen and (max-width: 992px) {
+          width: 100%;
+        }
+
+        .image-hidden {
+          display: none;
+        }
+
+      }
+      .list-group-item {
+        background-color: rgba(0, 0, 0, 0.4);
+        color: #f8f9fa;
+
+        &:hover {
+          background-color: rgba(255, 255, 255, 0.7);
+          cursor: pointer;
+          color: black;
+        }
+      }
+
+      @media screen and (max-width: 992px) {
+        display: flex;
+        padding: 15px;
       }
     }
     .plyr {
